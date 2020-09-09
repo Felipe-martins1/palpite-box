70046912051b29b44d4e4f278098785f86e021c7
Felipe <Felipe.dom.martins@gmail.com>
2020-09-09 13:24:47 -0300

    Arrumando erro do vercel

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Header from '../Header'
-import Footer from '../Footer'
+import React from './node_modules/react'
+import Header from '../header'
+import Footer from '../footer'
 
 const Layout = ({ children }) => {
     return (
