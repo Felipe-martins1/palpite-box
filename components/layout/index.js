a3ec4156607bf3b1c796ae621ed14851b8004501
Felipe <Felipe.dom.martins@gmail.com>
2020-12-02 16:23:23 -0300

    fix

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react'
+import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -57,7 +57,7 @@ const Pesquisa = () => {
                 <label className=' font-bold text-lg'>Nota:</label>
                 <div className='flex py-2'>
                     {notas.map(nota => {
-                        return (<label className='block w-1/3 '>
+                        return (<label>
                             {nota}<br />
                             <input type='radio' name='Nota' value={nota} onChange={onChange} />
                         </label>
